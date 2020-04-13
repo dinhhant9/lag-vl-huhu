8dffa6611bfe0ffc6d69b1af619fa8c6809e2e83
Dinh Han <dinhhant9@gmail.com>
2020-04-13 22:03:34 +0700

    first commit

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @yield('meta')
+  <title>Cẩm Nang Đấu Trường Chân Lý | @yield('title')</title>
+</head>
+<body>
+  <h1>aa</h1>
+</body>
+</html>
---
+++ resources/views/pages/trangchu.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+
+@section('title')
+Trang Chủ
+@endsection
+
+@section('meta')
+<meta name="description" content="Đấu Trường Chân Lý | Liên Minh Huyền Thoại">
+  <meta property="og:title" content="Đấu Trường Chân Lý | Liên Minh Huyền Thoại">
+  <meta property="og:description" content="Chiến đấu vì vinh quang">
+  <meta property="og:image" content="https://lienminh.garena.vn/images/Lan_h3lpm3/06_2019/DCTL/bg-hero.jpg">
+@endsection
