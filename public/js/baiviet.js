e0825b579bbca87a3d1c8af13cbd914bbc22d2f0
nguoingulanh <nguoingulanh@gmail.com>
2020-04-27 17:19:20 +0700

    Revert "Merge pull request #14 from dinhhant9/post-page"
    This reverts commit aaa376409b5c8e95c44a7cfc98474af8fa5a1b6c, reversing
    changes made to 74676b82dd4c7800e1f55fbd72bd2663cbb06a4c.

--- public/js/baiviet.js
+++
@@ -1,6 +0,0 @@
-let bannerElement = document.getElementById("banner");
-let patchElement = document.getElementById("patch");
-let bannerWidth = bannerElement.offsetWidth;
-if(bannerWidth < 860) {
-  patchElement.remove();
-} 
