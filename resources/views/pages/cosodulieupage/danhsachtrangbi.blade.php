a192b49bed12bd486407ffde9d66ee888754ab80
Vu Nguyen <vuhoangnguyen@gmail.com>
2020-05-26 22:01:49 +0700

    Danh sach trang bi ti chuot

--- resources/views/pages/cosodulieupage/danhsachtrangbi.blade.php
+++ resources/views/pages/cosodulieupage/danhsachtrangbi.blade.php
@@ -15,94 +15,265 @@ Danh Sách Trang Bị
 <meta property="og:image" content="https://lienminh.garena.vn/images/Lan_h3lpm3/06_2019/DCTL/bg-hero.jpg">
 @endsection
 @section('css')
-<link href="{{ asset('css/cosodulieu.css') }}" rel="stylesheet">
+<link href="{{ asset('css/danhsachtrangbi.css') }}" rel="stylesheet">
+
 {{-- Nội dung của trang --}}
 @endsection
 @section('content')
-<div class="row wrap-tab">
+<div class="row wrap-tab ">
     @include ('pages.cosodulieupage.menu')
-    <!-- item list tab content  -->
-    <div id="items" class="tabcontent">
-        <h2>TFT Items - Set 3</h2>
-        <div class="wrap-items">
-            <div class="base-items">
-                <h4>Base Items</h4>
-                <div class="list-base-items row">
-                    @for ($j = 0; $j < 10; $j++) <div class="wrap-item">
-                    
-                    @component('components.popup.trangbi')
-                    @endcomponent 
-                        <!-- <img class="image-base-items" src="{{ asset('images/items/GuardianAngel.png') }}" /> -->
-                        <!-- popup-info  -->
-                        <!-- <div class="popup-base-item">
-                            <div class="header">
-                                <img src="{{ asset('images/items/RabadonsDeathcap.png') }}" />
-                                <div class="wrap-item-name">
-                                    <div class="item-name">Mũ phù thủy Rabadon</div>
-                                    <div class="power">
-                                        <img src="{{ asset('images/items/icon-ap.svg') }}" />
-                                        <span>+40% </span>
+    <div class="main">
+            <div class="page-header">
+                    <h3>TFT  Item Tier List</h3>
+                    <div class="seachbar">
+                    <img class="searchbar-icon" src="https://rerollcdn.com/search-icon.svg" alt="search icon">
+                    <input type="text" placeholder="Seach...">
+
+                    </div>
+            </div>
+            <div class="divider"></div>
+            <div class="update mb-3">
+                <div class="update-patch">
+                    <h6 class="m-0" >Patch 10.10</h6>
+                </div>
+                <div class="legend d-flex">
+                    <div class="legend-item">
+                        <h6 class="m-0">Tier Up</h6>
+                    </div>
+                    <div class="legend-item">
+                        <h6 class="m-0">Tier Down</h6>
+                    </div>
+
+                </div>
+            </div>
+            <div class="tier-ground mb-3">
+                <div class="character-categorytone ">
+                    <h3>S</h3>
+                </div>
+               
+                
+                    <div class="character-list">
+                        @for ($j = 0; $j < 10; $j++)      
+                        <div class="character no-collapsable">                                  
+                                <div class="character-image">
+                                <img class="character-icon" src="https://rerollcdn.com/items/Spatula.png" alt="Spatula">
+                                </div>
+                        <!-- start popup  -->
+                                <div class="row character-popup"> 
+                                    <div class="col-md-3 col-3 character-info">
+                                        <img src="{{ asset('images/champions/Ekko.png') }}" />
+                                        <p>Ekko</p>
+                                    </div>
+                                    <div class="col-md-7 col-7 charactor-origin-wrap">
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/cybernetic.png') }}" />
+                                            <p>Siêu Công Nghệ</p>
+                                        </div>
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/infiltrator.png') }}" />
+                                            <p>Mật Thám</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-2 charactor-cost">
+                                        <img src="{{ asset('images/icon-gold.svg') }}" />
+                                        <div>5</div>
+                                    </div>
+                                    <div class="col-md-12 item">
+                                        <div>Items:</div>
+                                        <img src="{{ asset('images/items/GuardianAngel.png') }}" />
+                                        <img src="{{ asset('images/items/Morellonomicon.png') }}" />
+                                        <img src="{{ asset('images/items/SpearofShojin.png') }}" />
                                     </div>
                                 </div>
-                            </div>
-                            <div class="content">
-                                Tăng 25% sức mạnh phép thuật
-                            </div>
-                            <div class="footer">
-                                <div class="into">
-                                    Into:
-                                </div>
-                                <div class="list-items">
-                                    <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                    <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                    <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                    <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                </div>
-                            </div>
-                        </div> -->
-                        <!-- end popup-info  -->
-                </div>
-                @endfor
-            </div>
-        </div>
-        <div class="base-items">
-            <h4>Combinded Items</h4>
-            <div class="list-base-items row">
-                @for ($j = 0; $j < 30; $j++) <div class="wrap-item">
-                    <img class="image-base-items" src="{{ asset('images/items/GuardianAngel.png') }}" />
-                    <!-- popup-info  -->
-                    <div class="popup-base-item">
-                        <div class="header">
-                            <img src="{{ asset('images/items/RabadonsDeathcap.png') }}" />
-                            <div class="wrap-item-name">
-                                <div class="item-name">Mũ phù thủy Rabadon</div>
-                                <div class="power">
-                                    <img src="{{ asset('images/items/icon-ap.svg') }}" />
-                                    <span>+40% </span>
-                                </div>
-                            </div>
+                                
+                        <!-- end popup  -->
                         </div>
-                        <div class="content">
-                            Tăng 25% sức mạnh phép thuật
-                        </div>
-                        <div class="footer">
-                            <div class="into">
-                                Recipe:
-                            </div>
-                            <div class="list-items">
-                                <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                <img src="{{ asset('images/champions/Ekko.png') }}" />
-                                <img src="{{ asset('images/champions/Ekko.png') }}" />
-                            </div>
-                        </div>
+                        @endfor
                     </div>
-                    <!-- end popup-info  -->
+
+
             </div>
-            @endfor
-        </div>
+            <div class="tier-ground mb-3">
+                <div class="character-categorytthree ">
+                    <h3>A</h3>
+                </div>
+                <div class="character-list">
+                        @for ($j = 0; $j < 10; $j++)      
+                        <div class="character no-collapsable">                                  
+                                <div class="character-image">
+                                <img class="character-icon" src="https://rerollcdn.com/items/Spatula.png" alt="Spatula">
+                                </div>
+                        <!-- start popup  -->
+                                <div class="row character-popup"> 
+                                    <div class="col-md-3 col-3 character-info">
+                                        <img src="{{ asset('images/champions/Ekko.png') }}" />
+                                        <p>Ekko</p>
+                                    </div>
+                                    <div class="col-md-7 col-7 charactor-origin-wrap">
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/cybernetic.png') }}" />
+                                            <p>Siêu Công Nghệ</p>
+                                        </div>
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/infiltrator.png') }}" />
+                                            <p>Mật Thám</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-2 charactor-cost">
+                                        <img src="{{ asset('images/icon-gold.svg') }}" />
+                                        <div>5</div>
+                                    </div>
+                                    <div class="col-md-12 item">
+                                        <div>Items:</div>
+                                        <img src="{{ asset('images/items/GuardianAngel.png') }}" />
+                                        <img src="{{ asset('images/items/Morellonomicon.png') }}" />
+                                        <img src="{{ asset('images/items/SpearofShojin.png') }}" />
+                                    </div>
+                                </div>
+                                
+                        <!-- end popup  -->
+                        </div>
+                        @endfor
+            </div>
+           
+        </div>        
+    
+    <div class="tier-ground mb-3">
+                <div class="character-categoryttow">
+                    <h3>B</h3>
+                </div>
+                <div class="character-list">
+                        @for ($j = 0; $j < 10; $j++)      
+                        <div class="character no-collapsable">                                  
+                                <div class="character-image">
+                                <img class="character-icon" src="https://rerollcdn.com/items/Spatula.png" alt="Spatula">
+                                </div>
+                        <!-- start popup  -->
+                                <div class="row character-popup"> 
+                                    <div class="col-md-3 col-3 character-info">
+                                        <img src="{{ asset('images/champions/Ekko.png') }}" />
+                                        <p>Ekko</p>
+                                    </div>
+                                    <div class="col-md-7 col-7 charactor-origin-wrap">
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/cybernetic.png') }}" />
+                                            <p>Siêu Công Nghệ</p>
+                                        </div>
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/infiltrator.png') }}" />
+                                            <p>Mật Thám</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-2 charactor-cost">
+                                        <img src="{{ asset('images/icon-gold.svg') }}" />
+                                        <div>5</div>
+                                    </div>
+                                    <div class="col-md-12 item">
+                                        <div>Items:</div>
+                                        <img src="{{ asset('images/items/GuardianAngel.png') }}" />
+                                        <img src="{{ asset('images/items/Morellonomicon.png') }}" />
+                                        <img src="{{ asset('images/items/SpearofShojin.png') }}" />
+                                    </div>
+                                </div>
+                                
+                        <!-- end popup  -->
+                        </div>
+                        @endfor
+            </div>
+    </div> 
+    <div class="tier-ground mb-3">
+                <div class="character-categorytfour">
+                    <h3>C</h3>
+                </div>
+                <div class="character-list">
+                        @for ($j = 0; $j < 10; $j++)      
+                        <div class="character no-collapsable">                                  
+                                <div class="character-image">
+                                <img class="character-icon" src="https://rerollcdn.com/items/Spatula.png" alt="Spatula">
+                                </div>
+                        <!-- start popup  -->
+                                <div class="row character-popup"> 
+                                    <div class="col-md-3 col-3 character-info">
+                                        <img src="{{ asset('images/champions/Ekko.png') }}" />
+                                        <p>Ekko</p>
+                                    </div>
+                                    <div class="col-md-7 col-7 charactor-origin-wrap">
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/cybernetic.png') }}" />
+                                            <p>Siêu Công Nghệ</p>
+                                        </div>
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/infiltrator.png') }}" />
+                                            <p>Mật Thám</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-2 charactor-cost">
+                                        <img src="{{ asset('images/icon-gold.svg') }}" />
+                                        <div>5</div>
+                                    </div>
+                                    <div class="col-md-12 item">
+                                        <div>Items:</div>
+                                        <img src="{{ asset('images/items/GuardianAngel.png') }}" />
+                                        <img src="{{ asset('images/items/Morellonomicon.png') }}" />
+                                        <img src="{{ asset('images/items/SpearofShojin.png') }}" />
+                                    </div>
+                                </div>
+                                
+                        <!-- end popup  -->
+                        </div>
+                        @endfor
+            </div>
     </div>
-    <!-- end item list tab content  -->
-</div>
+    <div class="tier-ground mb-3">
+                <div class="character-categorytfive">
+                    <h3>D</h3>
+                </div>
+                <div class="character-list">
+                        @for ($j = 0; $j < 10; $j++)      
+                        <div class="character no-collapsable">                                  
+                                <div class="character-image">
+                                <img class="character-icon" src="https://rerollcdn.com/items/Spatula.png" alt="Spatula">
+                                </div>
+                        <!-- start popup  -->
+                                <div class="row character-popup"> 
+                                    <div class="col-md-3 col-3 character-info">
+                                        <img src="{{ asset('images/champions/Ekko.png') }}" />
+                                        <p>Ekko</p>
+                                    </div>
+                                    <div class="col-md-7 col-7 charactor-origin-wrap">
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/cybernetic.png') }}" />
+                                            <p>Siêu Công Nghệ</p>
+                                        </div>
+                                        <div class="charactor-origin">
+                                            <img src="{{ asset('images/origin/infiltrator.png') }}" />
+                                            <p>Mật Thám</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 col-2 charactor-cost">
+                                        <img src="{{ asset('images/icon-gold.svg') }}" />
+                                        <div>5</div>
+                                    </div>
+                                    <div class="col-md-12 item">
+                                        <div>Items:</div>
+                                        <img src="{{ asset('images/items/GuardianAngel.png') }}" />
+                                        <img src="{{ asset('images/items/Morellonomicon.png') }}" />
+                                        <img src="{{ asset('images/items/SpearofShojin.png') }}" />
+                                    </div>
+                                </div>
+                                
+                        <!-- end popup  -->
+                        </div>
+                        @endfor
+            </div>
+
+
+    </div> 
+ 
+        
+    </div> 
+</div>      
+
+
 
 @endsection
